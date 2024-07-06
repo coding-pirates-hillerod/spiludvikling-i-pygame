26f162a789a9fe1ff5bb135b809051a3ebb0d617
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-06 13:15:20 +0200

    Added skeleton for coding ones own game

---
+++ 01-aften/03-exercises/01-code-your-own-game-from-scratch/main.py
@@ -0,0 +1,10 @@
+# Step 1 - Importer og initialisering pygame
+
+
+# Step 2 - Definer dit "Game Window"
+
+
+# Step 3 - Initialiser dit "Game Loop"
+
+
+# Step 4 - Tjek for events (her for pygames "QUIT" event)
