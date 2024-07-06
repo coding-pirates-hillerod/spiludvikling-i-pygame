62f7e51457eb064c2f75c1408a2865d21d09a66c
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-06 14:56:52 +0200

    Added missing 'screen' variabel and added initial example for adding elements to game window

--- 02-aften/02-examples/02-adding-elements-to-game-window/main.py
+++ 02-aften/02-examples/02-adding-elements-to-game-window/main.py
@@ -1,21 +1,15 @@
-"""Koding af spil i Pygame foregår som udgangspunkt via følgende 4 steps"""
-
-# Step 1 - Import og initialering af Pygame
 import pygame
 
 pygame.init()
 
-# Step 2 - Definition af ens "Game Window"
 SCREE_WIDTH = 800
 SCREE_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
 
-# Step 3 - Afvikling af "Game Loop"
 run = True
 while run:
 
-    # Step 4 - Tjek for events i ens "Event Handler"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
