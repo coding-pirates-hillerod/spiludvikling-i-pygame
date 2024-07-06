575fd9b9ad34a14690a992a020c987dcab0a6619
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-06 15:02:48 +0200

    Added comments with steps on how to added elements to ones game window

--- 02-aften/02-examples/02-adding-elements-to-game-window/main.py
+++ 02-aften/02-examples/02-adding-elements-to-game-window/main.py
@@ -7,11 +7,17 @@ SCREE_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
 
+# Step 1 - Tilføj dit element her (før dit "Game Loop")
+
 run = True
 while run:
+
+    # Step 2 - Tegn dit element på dit "Game Window" her
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # Step 3 - Opdater dit "Game Window", så elementet tegnes
 
 pygame.quit()
