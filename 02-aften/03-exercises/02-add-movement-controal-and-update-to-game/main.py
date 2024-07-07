9b31ba55109bd902827e9ae6c5713bcd1ba0b63c
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-07 10:40:45 +0200

    Added 02-exercise

---
+++ 02-aften/03-exercises/02-add-movement-controal-and-update-to-game/main.py
@@ -0,0 +1,25 @@
+"""Tilføj keyboard kontrol og korrekt opdatering af skærm til dette spil"""
+
+import pygame
+
+pygame.init()
+
+SCREE_WIDTH = 800
+SCREE_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
+
+rectangle = pygame.Rect((300, 250, 50, 50))
+
+run = True
+while run:
+
+    pygame.draw.rect(screen, (255, 0, 0), rectangle)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
