25fe243290f3f4151adb661b6842bf0dda743d83
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-07 10:33:40 +0200

    Added correct movement handling and update of screen

---
+++ 02-aften/02-examples/11-update-game-window-correct/main.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+
+SCREE_WIDTH = 800
+SCREE_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
+
+rectangle = pygame.Rect((300, 250, 50, 50))
+
+run = True
+while run:
+
+    # Opdater "Game Window" korrekt
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, (255, 0, 0), rectangle)
+
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_UP]:
+        rectangle.move_ip(0, -1)
+    elif key[pygame.K_DOWN]:
+        rectangle.move_ip(0, 1)
+    elif key[pygame.K_LEFT]:
+        rectangle.move_ip(-1, 0)
+    elif key[pygame.K_RIGHT]:
+        rectangle.move_ip(1, 0)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
