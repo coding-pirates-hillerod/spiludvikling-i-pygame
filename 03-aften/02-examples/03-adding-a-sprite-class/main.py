831ad937194942f58cad083f3aef3927d5724b00
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-11 08:41:04 +0200

    Added examples

--- 03-aften/02-examples/03-adding-a-sprite-class/main.py
+++ 03-aften/02-examples/03-adding-a-sprite-class/main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import _Group
 
 pygame.init()
 
@@ -8,7 +9,14 @@ SCREE_HEIGHT = 600
 screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
 
 
-# Step 1 - lav en Python klasse til din Sprite
+# Step 1 - Klasse til din Sprite - her et "Spaceship"
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("./spaceship.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [400, 550]
+
 
 # Step 2 - Initialiser Sprite og Sprite Group
 
