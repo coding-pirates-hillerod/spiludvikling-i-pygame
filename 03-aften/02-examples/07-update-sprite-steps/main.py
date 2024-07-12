2da3ee8e99e3d4a552ce761ed3767aecedd00a42
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-07-12 15:27:39 +0200

    Added example 07

---
+++ 03-aften/02-examples/07-update-sprite-steps/main.py
@@ -0,0 +1,41 @@
+import pygame
+
+pygame.init()
+
+SCREE_WIDTH = 800
+SCREE_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREE_WIDTH, SCREE_HEIGHT))
+
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("./spaceship.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (400, 550)
+
+    # Step 1 - overskriv "update" metoden her
+
+
+spaceship = Spaceship()
+spaceship_group = pygame.sprite.Group()
+
+spaceship_group.add(spaceship)
+
+run = True
+while run:
+
+    screen.fill((0, 0, 0))
+
+    # Step 2 - kald "update" metoden på din sprite group
+
+    spaceship_group.draw(screen)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
